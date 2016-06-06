47ecb580c4fc0b404cb1c8e947c9acdab18ed458
Nelson Scott <officialbadbeargames@gmail.com>
2016-06-06 19:38:18 -0400

    Init commit

---
+++ Assets/Scripts/KillScott.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillScott : MonoBehaviour 
+{
+	private ParticleSystem fireSystem;
+
+	void Start () 
+	{
+		fireSystem = GetComponent<ParticleSystem>();
+		fireSystem.Stop();
+	}
+	
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (fireSystem.isPlaying)
+			{
+				fireSystem.Stop();
+			}
+			else
+			{
+				fireSystem.Play();
+			}
+		}
+	}
+}
