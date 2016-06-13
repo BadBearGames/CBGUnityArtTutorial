b12d94f1bb9218a1bc361b31d213bc3991bae692
Nelson Scott <officialbadbeargames@gmail.com>
2016-06-13 14:32:19 -0400

    revision

---
+++ Assets/Scripts/AnimateScott.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateScott : MonoBehaviour 
+{
+	Animator animator;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			animator.SetBool("running", !animator.GetBool("running"));
+		}
+	}
+}
